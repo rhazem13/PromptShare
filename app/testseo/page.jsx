28f51f83c13ba6fb91d3873712d4b211d77933ab
rhazem13 <rhazem13@yahoo.com>
2023-08-06 23:09:11 +0300

    test2

--- app/testseo/page.jsx
+++ app/testseo/page.jsx
@@ -1,30 +1,40 @@
 import Head from "next/head";
 
-const TestSEOPage = () => {
+const TestSEOPage = ({ data }) => {
   return (
     <div>
       <Head>
-        <title>CSR Test Page | My Next.js App</title>
+        <title>SSR Test Page | My Next.js App</title>
         <meta
           name="description"
-          content="This is a test page for client-side rendering (CSR) SEO."
+          content="This is a test page for server-side rendering (SSR) SEO."
         />
-        <meta name="keywords" content="CSR, SEO, test, Next.js, keywords" />
+        <meta name="keywords" content="SSR, SEO, test, Next.js, keywords" />
         <meta name="author" content="Your Name" />
         <meta name="robots" content="index, follow" />
-        <meta property="og:title" content="CSR Test Page" />
+        <meta property="og:title" content="SSR Test Page" />
         <meta
           property="og:description"
-          content="This is a test page for client-side rendering (CSR) SEO."
+          content="This is a test page for server-side rendering (SSR) SEO."
         />
         <meta property="og:image" content="/images/seo-image.jpg" />
       </Head>
 
-      <h1>Client-Side Rendering (CSR) Test Page</h1>
-      <p>This is a test page for client-side rendering (CSR) SEO.</p>
+      <h1>Server-Side Rendering (SSR) Test Page</h1>
+      <p>This is a test page for server-side rendering (SSR) SEO.</p>
+      <p>Data from the server: {data}</p>
       <img src="/images/seo-image.jpg" alt="SEO Image" />
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const data = "Sample data from server"; // Simulated data from the server
+  return {
+    props: {
+      data,
+    },
+  };
+}
 
 export default TestSEOPage;
